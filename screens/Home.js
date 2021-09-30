8ba7ac772575a32ae91a64d46c28ec32eb2b2d64
Njengak11 <njengak11@gmail.com>
2021-09-30 14:20:52 +0300

    added icons

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity, Image } from 'react-native';
 import Carousel from 'react-native-card-carousel-animated';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 
 const Home = ({navigation}) => {
@@ -14,10 +15,12 @@ const Home = ({navigation}) => {
             id: '1',
             child: (
                 <View style={styles.cards}>
+                    <Text style={styles.cardText}>Happy</Text>
                     <Image 
                     style={styles.image}
                     source={require('../assets/images/happy.png')}
                     />
+                    
                 </View>
             ),
         },
@@ -25,6 +28,7 @@ const Home = ({navigation}) => {
             id: '2',
             child: (
                 <View style={styles.cards}>
+                    <Text style={styles.cardText}>Sad</Text>
                     <Image 
                     style={styles.image}
                     source={require('../assets/images/sad.png')}
@@ -36,6 +40,7 @@ const Home = ({navigation}) => {
             id: '3',
             child: (
                 <View style={styles.cards}>
+                    <Text style={styles.cardText}>Angry</Text>
                     <Image 
                     style={styles.image}
                     source={require('../assets/images/angry.png')}
@@ -47,6 +52,7 @@ const Home = ({navigation}) => {
             id: '4',
             child: (
                 <View style={styles.cards}>
+                    <Text style={styles.cardText}>Surprised</Text>
                     <Image 
                     style={styles.image}
                     source={require('../assets/images/surprise.png')}
@@ -58,6 +64,7 @@ const Home = ({navigation}) => {
             id: '5',
             child: (
                 <View style={styles.cards}>
+                    <Text style={styles.cardText}>Fear</Text>
                     <Image 
                     style={styles.image}
                     source={require('../assets/images/fear.png')}
@@ -69,6 +76,7 @@ const Home = ({navigation}) => {
             id: '6',
             child: (
                 <View style={styles.cards}>
+                    <Text style={styles.cardText}>Relaxed</Text>
                     <Image 
                     style={styles.image}
                     source={require('../assets/images/relaxed.png')}
@@ -80,6 +88,7 @@ const Home = ({navigation}) => {
             id: '7',
             child: (
                 <View style={styles.cards}>
+                    <Text style={styles.cardText}>Tired</Text>
                     <Image 
                     style={styles.image}
                     source={require('../assets/images/tired.png')}
@@ -97,15 +106,24 @@ const Home = ({navigation}) => {
             <Carousel cards={data} />
             </View>
 
+            <View style={{height:5}}></View>
+
             <View style={styles.explainContainer}>
                 <Text style={styles.explainText}>Why do you feel this way?</Text>
 
-                <View style={styles.explainArea}></View>
+                <View style={styles.explainArea}>
+                    
+                </View>
 
                 <TouchableOpacity style={styles.journalBtn}
         onPress={onPressHandler}
         >
           <Text style={styles.btnText}>Go to journal</Text>
+          <FontAwesome style={styles.icon}
+          name='angle-right'
+          size={40}
+          color='white'
+          />
         </TouchableOpacity>
             </View>
         </View>
@@ -120,7 +138,7 @@ const styles = StyleSheet.create({
     },
     subtitle:{
         color:'white',
-        fontSize:18,
+        fontSize:24,
         paddingTop:25,
         paddingLeft:20
     },
@@ -132,8 +150,17 @@ const styles = StyleSheet.create({
     },
     cards:{
         backgroundColor:'#7d7a9f',
-    width: '100%',
-    height: '100%',
+        width: '100%',
+        height: '100%',
+        paddingTop:18,
+        paddingBottom:22,
+        alignItems:'center',
+        
+    },
+    cardText:{
+        color:'white',
+        top:132,
+        
     },
     image:{
         flex: 1,
@@ -146,7 +173,7 @@ const styles = StyleSheet.create({
         paddingBottom:200,
         borderTopLeftRadius:30,
         borderTopRightRadius:30,
-        bottom:80
+        bottom:100
     },
     explainArea:{
         backgroundColor:'white',
@@ -154,7 +181,7 @@ const styles = StyleSheet.create({
         marginLeft:35,
         marginRight:35,
         paddingBottom:40,
-        height:270,
+        height:295,
         top:30
     },
     explainText:{
@@ -162,7 +189,7 @@ const styles = StyleSheet.create({
         color:'#7d7a9f',
         paddingTop:15,
         paddingLeft:20,
-        paddingBottom:10
+        paddingBottom:10,
     },
     journalBtn:{
         width:'100%',
@@ -170,7 +197,7 @@ const styles = StyleSheet.create({
         borderTopRightRadius:30,
         alignItems:'center',
         justifyContent:'center',
-        marginTop:100,
+        marginTop:80,
         backgroundColor:'white',
         paddingLeft:40,
         height:70,
@@ -179,7 +206,12 @@ const styles = StyleSheet.create({
     btnText:{
         color:'white',
         fontSize:18,
+        top:18,
+        alignItems:'center'
     },
-    
+    icon:{
+        left:130,
+        bottom:12
+    }
 })
 export default Home;
